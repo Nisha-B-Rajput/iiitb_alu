e79aff6cbde6265575bed2b25bed25d75d06b0f0
Nisha-B-Rajput <110079800+Nisha-B-Rajput@users.noreply.github.com>
2022-08-04 18:39:10 +0530

    Update yosys_alu.sh

--- yosys_alu.sh
+++ yosys_alu.sh
@@ -1,1 +1,22 @@
+# read design
+read_verilog iiitb_alu.v
 
+
+
+# generic synthesis
+synth -top iiitb_alu
+
+
+
+# mapping to mycells.lib
+dfflibmap -liberty /home/nisha/ALU/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/nisha/ALU/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
+
+clean
+
+flatten
+
+# write synthesized design
+write_verilog -noattr iiitb_alu_synth.v 
+
+stat
